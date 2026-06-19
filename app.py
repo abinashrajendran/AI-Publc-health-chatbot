9d8b8f60e128b69890da31a7902a0d31fa33da9d
abinashrajendran <abiakash0804@gmail.com>
2026-06-19 10:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 print("AI Public Health Chatbot")
+
+user_input = input("Enter your symptom: ")
+
+if user_input.lower() == "fever":
+    print("Drink plenty of water and consult a doctor if symptoms persist.")
+else:
+    print("Please consult a healthcare professional for advice.")
